9c68156590e555f0f980f892486bda9e528182d8
freddycro <freddy10100@hotmail.com>
2019-04-11 19:50:45 +0800

    synthesis image

--- index.php
+++ index.php
@@ -34,12 +34,12 @@ require_once './vendor/autoload.php';
 require_once './src/autoloader.php';
 
 $config = [
-    // Your driver-specific configuration
-    'facebook' => [
-      'token' => 'EAAgFGCRdh0YBAO6YOEFzHACkJPmpWCXY60AgtsoBO3cotn0C32fklDurz6nscGZCvS83BasSXrZAtjWjGXhq86wsMV2QhsoN4XYlucR6uMxufZBCEIUOdL3ZB5TPhaAq3EUAkhmXnZCPSkVXVWDoQnz1wBjeo69NvOKfwQJn5nQZDZD',
-      'app_secret' => 'ee950085cfeacdd271ebaad5be3672aa',
-      'verification'=>'happymothersdayudnforyou',
-    ]
+  // Your driver-specific configuration
+  'facebook' => [
+    'token' => 'EAAgFGCRdh0YBAO6YOEFzHACkJPmpWCXY60AgtsoBO3cotn0C32fklDurz6nscGZCvS83BasSXrZAtjWjGXhq86wsMV2QhsoN4XYlucR6uMxufZBCEIUOdL3ZB5TPhaAq3EUAkhmXnZCPSkVXVWDoQnz1wBjeo69NvOKfwQJn5nQZDZD',
+    'app_secret' => 'ee950085cfeacdd271ebaad5be3672aa',
+    'verification'=>'happymothersdayudnforyou',
+  ]
 ];
 
 // Load the driver(s) you want to use
@@ -50,44 +50,66 @@ DriverManager::loadDriver(\BotMan\Drivers\Facebook\FacebookImageDriver::class);
 $botman = BotManFactory::create($config);
 $textBoolean = 0;
 $imageBoolean = 0;
+$textContent = '';
+$imageUrl = '';
 
-// $botman->say('來寫母親節卡片吧');
-// $botman->hears('hello', function (BotMan $bot) {
-//   $bot->reply('hi');
-// });
+function saveImage($srcUrl, $distUrl, $fileName) {
+  $urlToImage = $srcUrl;
+  $completeSaveLoc = $distUrl . $fileName;
+  file_put_contents($completeSaveLoc, file_get_contents($urlToImage));
+}
+
+function imageSynthesis($srcTitle, $srcText, $srcImage) {
+  $editor = Grafika::createEditor();
+  $editor->open($image1, 'card_materials/background.png');
+  $editor->open($image2, $srcImage);
+  $editor->blend($image1, $image2 , 'normal', 0.9, 'center');
+  $editor->text($image1, $srcText, 36, 200, 200, null, '', 45);
+  $editor->save($image1, 'users_data/mothersCard.png');
+}
 
 
 
 
+
+
+  
 $botman->hears('to mom {text}', function(BotMan $bot, $text) {
+  global $textBoolean, $imageBoolean, $textContent, $imageUrl;
+
   // $bot->reply($text);
+  $textContent = $text;
   $bot->reply(Question::create('確定給媽媽的話?')->addButtons([
     Button::create('Yes')->value('text yes'),
     Button::create('No')->value('text no'),
   ]));
-
-  // $bot->startConversation(new CardConversation);
 });
-
+  
 $botman->receivesImages(function(BotMan $bot, $images) {
-  foreach ($images as $image) {
-      $url=$image->getUrl();
-      $title=$image->getTitle();
-      // $bot->reply($url);
-      $editor = Grafika::createEditor();
-      $editor->open($image1 , '1.png');
-      $editor->resizeFit($image1 , 200 , 200);
-      $editor->save($image1 , '2.png');
-  }
+  global $textBoolean, $imageBoolean, $textContent, $imageUrl;
 
+  foreach ($images as $image) {
+    $url=$image->getUrl();
+    // $title=$image->getTitle();
+    // $bot->reply($url);
+    $imageUrl = $url;
+  }
+  
   $bot->reply(Question::create('確定卡片的圖片?')->addButtons([
     Button::create('Yes')->value('image yes'),
     Button::create('No')->value('image no'),
   ]));
 });
 
+
+
+
+
+    
 $botman->hears('text yes', function (BotMan $bot) {
+  global $textBoolean, $imageBoolean, $textContent, $imageUrl;
   $textBoolean = 1;
+
   // 如果圖文都上傳，詢問是否要開始製作卡片
   if ($textBoolean = 1 && $imageBoolean = 1) {
     $bot->reply(Question::create('要開始製作卡片了嗎?')->addButtons([
@@ -102,7 +124,12 @@ $botman->hears('text yes', function (BotMan $bot) {
 });
 
 $botman->hears('image yes', function (BotMan $bot) {
+  global $textBoolean, $imageBoolean, $textContent, $imageUrl;
+
   $imageBoolean = 1;
+  // 這邊檔名要加上id，才不會覆蓋
+  saveImage($imageUrl, 'users_data/', 'userImage.png');
+
   // 如果圖文都上傳，詢問是否要開始製作卡片
   if ($textBoolean = 1 && $imageBoolean = 1) {
     $bot->reply(Question::create('要開始製作卡片了嗎?')->addButtons([
@@ -112,16 +139,24 @@ $botman->hears('image yes', function (BotMan $bot) {
   } else {
     $bot->reply('照片選好囉');
   }
-
-  // 寫進資料庫，不再寫入圖片
+    // 寫進資料庫，不再寫入圖片
 });
-
+  
 $botman->hears('all yes', function (BotMan $bot) {
-  $bot->reply('全部輸入完成');
+  global $textBoolean, $imageBoolean, $textContent, $imageUrl;
+
   $textBoolean = 0;
   $imageBoolean = 0;
+  // imageSynthesis('Title', $textContent, 'users_data/userImage.png');
+  $bot->reply('全部輸入完成');
   // 寫進資料庫，不再寫入文字
 });
+
+
+
+
+
+
 
 // Start listening
 $botman->listen();
